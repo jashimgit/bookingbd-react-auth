2d30d5690087d66581febc1dbfa7986efdf9641f
jashimgit <jashim.ncn@gmail.com>
2021-04-27 01:25:57 +0600

    firebaseconfig file updated with env

--- src/components/FirebaseConfig/FirebaseConfig.js
+++ src/components/FirebaseConfig/FirebaseConfig.js
@@ -1,11 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDWQOLuN6yxsvSEacpaVr8_ChfQvCEszzs",
-    authDomain: "bookingbd-8468d.firebaseapp.com",
-    projectId: "bookingbd-8468d",
-    storageBucket: "bookingbd-8468d.appspot.com",
-    messagingSenderId: "401269879744",
-    appId: "1:401269879744:web:ff827514da9b517b75ce25"
-  };
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+};
 
-
-  export default firebaseConfig;
+export default firebaseConfig;
